0e84e7639fcd0c06516aec300ab9d71216b7e9f7
Sushant Shankar <2140317@sliet.ac.in>
2024-09-22 00:59:45 +0530

    Model Trainer

--- src/utils.py
+++ src/utils.py
@@ -2,7 +2,7 @@ import os
 import sys 
 import numpy as np
 import pandas as pd
-import dill
+import dill # type: ignore
 from sklearn.metrics import r2_score
 
 from src.exception import CustomException
